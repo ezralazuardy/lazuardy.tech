497c7f14a86e12e59d266db9e7704e3718fcd7ad
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-08-28 15:22:04 +0700

    feat: update

--- components/ui/footer.js
+++ components/ui/footer.js
@@ -36,7 +36,7 @@ export default forwardRef(function Footer(props, ref) {
         <div className="absolute top-0 left-0 w-screen">
           <Meteors number={30} />
         </div>
-        <div className="w-full px-14 text-white">
+        <div className="w-full px-16 mb-16 text-white">
           <Logo className="w-full text-start text-white text-xl mb-20" />
           <div className="w-full text-start text-white text-4xl mb-10 font-medium uppercase tracking-wide leading-snug">
             <span>
@@ -51,28 +51,33 @@ export default forwardRef(function Footer(props, ref) {
                   simply have a chat.
                 </span>
               </div>
-              <div className="text-white text-xl font-light flex gap-3">
+              <div className="text-white text-xl font-light flex gap-2 hover:gap-4 transition-all">
                 <Link href="mailto:contact@lazuardy.tech">
                   contact@lazuardy.tech
                 </Link>
-                <ArrowRight className="w-6 h-6 mt-1" />
+                <ArrowRight className="w-5 h-5 mt-1.5" />
               </div>
             </div>
             <div className="w-full text-start col-span-1"></div>
-            <div className="w-full text-start col-span-2">
+            <div className="w-full text-start col-span-2 pt-2">
               <div className="text-white text-md mb-4 font-medium uppercase">
-                <span>Partnership</span>
+                <span>Our Address</span>
               </div>
-              <div className="text-gray-300 text-md font-light">
-                Get more profit based on the project value that you can give to
-                us, just send an email if you&apos;re interested.
+              <div className="text-gray-400 text-md font-light">
+                Menara Suara Merdeka
+                <br />
+                Jl. Pandanaran No.30
+                <br />
+                Semarang, 50134
+                <br />
+                Indonesia
               </div>
             </div>
-            <div className="w-full text-start col-span-1">
+            <div className="w-full text-start col-span-1 pt-2">
               <div className="text-white text-md mb-4 font-medium uppercase">
                 <span>Follow us</span>
               </div>
-              <div className="text-gray-300 text-md font-light mb-2 hover:underline">
+              <div className="text-gray-400 text-md font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer">
                 <Link
                   href="https://instagram.com/lazuardy.tech"
                   target="_blank"
@@ -80,7 +85,7 @@ export default forwardRef(function Footer(props, ref) {
                   Instagram
                 </Link>
               </div>
-              <div className="text-gray-300 text-md font-light mb-2 hover:underline">
+              <div className="text-gray-400 text-md font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer">
                 <Link
                   href="https://linkedin.com/company/lazuardy/?viewAsMember=true"
                   target="_blank"
@@ -88,29 +93,39 @@ export default forwardRef(function Footer(props, ref) {
                   LinkedIn
                 </Link>
               </div>
-              <div className="text-gray-300 text-md font-light mb-2 hover:underline">
+              <div className="text-gray-400 text-md font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer">
                 <Link href="https://blog.lazuardy.tech" target="_blank">
                   Medium
                 </Link>
               </div>
             </div>
-            <div className="w-full text-start col-span-1 text-white text-md font-light">
+            <div className="w-full text-start col-span-1 pt-2 text-white text-md font-light">
               <div
-                className="text-gray-300 mb-2 hover:underline hover:cursor-pointer"
+                className="text-white font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer"
                 onClick={redirectHome}
               >
-                home
+                <span className="text-white text-xs mr-2">▶</span>
+                <span>home</span>
               </div>
               <div
-                className="text-gray-300 mb-2 hover:underline hover:cursor-pointer"
+                className="text-gray-400 font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer"
                 onClick={redirectContact}
               >
-                contact
+                <span className="text-black text-xs mr-2">▶</span>
+                <span>contact</span>
               </div>
-              <div className="text-gray-300 mb-2 hover:underline">
+              <div className="text-gray-400 font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer">
                 <Link href="https://blog.lazuardy.tech" target="_blank">
-                  article
+                  <span className="text-black text-xs mr-2">▶</span>
+                  <span>articles</span>
                 </Link>
+              </div>
+              <div
+                className="text-gray-400 font-medium mb-2 transition-colors hover:text-white hover:cursor-pointer"
+                onClick={redirectContact}
+              >
+                <span className="text-black text-xs mr-2">▶</span>
+                <span>partnership</span>
               </div>
             </div>
           </div>
